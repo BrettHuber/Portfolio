27dffc39d13f856c7f9bf11b370cd827708af324
B H <hawktalon505@gmail.com>
2022-05-14 00:13:08 -0400

    Fixes

--- portfolio-website/src/components/VideoSection.js
+++ portfolio-website/src/components/VideoSection.js
@@ -5,13 +5,16 @@ import '../App';
 function VideoSection () {
     return (
         <div className = "home-video">
-            <video src = "/videos/Code_flythough_loop_01_Videvo.mov" autoPlay loop muted ></video>
-            <div className="box">
-                <h1>Portfolio</h1>
-                <p>By Brett Huber</p>
-            </div>
+         
         </div>
     )
 }
 
 export default VideoSection
+
+/*            <video src = "/videos/Code_flythough_loop_01_Videvo.mov" autoPlay loop muted ></video>
+   <div className="box">
+                <h1>Portfolio</h1>
+                <p>By Brett Huber</p>
+            </div>
+*/
