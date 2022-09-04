e2c3fcc6c85dc336f02becdd68b5549d19e46312
B H <hawktalon505@gmail.com>
2022-09-04 00:31:39 -0400

    Updated home page

--- portfolio-website/src/components/Webpages/Home.js
+++ portfolio-website/src/components/Webpages/Home.js
@@ -1,18 +1,21 @@
 import { Component } from 'react';
 import '../../App.css';
-import VideoSection from '../VideoSection'
+import './Home.css'
+
 
 class Home extends Component {
     render() {
         return (
-        <div>
-            <div className="banner-container">
+        
+            <div className="home-container">
                 <div className="text-center">
+                    <br></br>
+                    <img src =  'images/Circle-Headshot.png' className = "headshot" alt = "Brett"></img>
                     <h1 className= "homeName">Brett Huber</h1>
-                    <h4 className="homeAttributes">Student, Programmer, Security Analyst</h4>
+                    <h4 className="homeAttributes">Student, Programmer, & Security Analyst</h4>
                 </div>
             </div>
-        </div>  
+         
         );
     }
 }
