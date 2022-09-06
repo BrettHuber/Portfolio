319024568f899f19dd31d65f7c6cd28939adb361
B H <hawktalon505@gmail.com>
2022-09-06 18:05:23 -0400

    Adjusted Contact.js

--- portfolio-website/src/components/Contact/Contact.js
+++ portfolio-website/src/components/Contact/Contact.js
@@ -10,9 +10,14 @@ render() {
     return (
     <div className = "Contact-Form">
       <Form>
-        <Form.Field id = 'form-input-control-email' control = {Input} label = 'Email' name = 'user_email' placeholder = 'Your Email' required icon = 'mail' iconPosition = 'left'/>
-        <Form.Field id = 'form-input-control-last-name' control = {Input} label ='Name' name = 'user_name' placeholder = 'Your Name' required icon = 'user circle' iconPosition = 'left'/>
-        <Form.Field id = 'form-textarea-control-opinion' control ={TextArea} label = 'Message' name = 'user_message' placeholder = 'Message' required/>
+        <label for = "Email">Email: </label>
+        <Form.Field id = 'input-email' control = {Input} name = 'user_email' placeholder = 'Email...' required icon = 'mail' iconPosition = 'left'/>
+        <label for = "Name">Name: </label>
+        <Form.Field id = 'input-name' control = {Input} name = 'user_name' placeholder = 'Name...' required icon = 'user circle' iconPosition = 'left'/>
+        <label for = "Subject">Subject: </label>
+        <Form.Field id = 'input-subject' control ={Input} name = 'user_subject' placeholder = 'Subject...' required/>
+        <label for = "Subject">Message: </label>
+        <Form.Field id = 'form-textarea-control-opinion' control ={TextArea} name = 'user_message' placeholder = 'Message...' required/>
         <Button type = 'submit' color = 'green'>Submit</Button>
       </Form>
     </div>
