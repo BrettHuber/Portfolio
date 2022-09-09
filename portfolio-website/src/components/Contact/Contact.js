2a514642e20ee7d09acbae08586bd72c3c3db6d1
B H <hawktalon505@gmail.com>
2022-09-09 00:57:10 -0400

    Adjusted contact form

--- portfolio-website/src/components/Contact/Contact.js
+++ portfolio-website/src/components/Contact/Contact.js
@@ -8,36 +8,94 @@ class Contact extends Component {
 
 handleSubmit = async (event) => {
     event.preventDefault();
+    // emailjs.init("bJBby5TiIur5Br_SS");
+    // SID: event('S_ID')
+    // TID: {"process.env.T_ID"}
+    // UID: {"process.env.S_ID"}
+
+
+   emailjs.sendForm('service_ewmc7h9', 'template_0l44cv7', event.target, 'bJBby5TiIur5Br_SS').then(function(response) {
+      // emailjs.sendForm(SID, {process.env.T_ID}, event.target, UID).then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+      console.log('FAILED...', error);
+    });
     Swal.fire({
       title: "Email Submitted",
       text: "Message Sent!",
       icon: "success",
       button: "Close",
     });
-
-    
+    event.target.reset();
 };
 
 render() {
     return (
     <div className = "Contact-Form">
-      <Form onSubmit={this.handleSubmit}>
+      <Form onSubmit = {this.handleSubmit}>
         <h1>Contact Me</h1>
-        <label className = "label" for = "Email">Email: </label>
-        <Form.Field id = 'input-email' control = {Input} name = 'user_email' placeholder = 'Email...' required icon = 'mail' iconPosition = 'left'/>
-        <br></br>
-        <label className = "label" for = "Name">Name: </label>
-        <Form.Field id = 'input-name' control = {Input} name = 'user_name' placeholder = 'Name...' required icon = 'user circle' iconPosition = 'left'/>
-        <br></br>
-        <label className = "label" for = "Subject">Subject: </label>
-        <Form.Field id = 'input-subject' control ={Input} name = 'user_subject' placeholder = 'Subject...' required/>
-        <br></br>
-        <label className = "label" for = "Message">Message: </label>
-        <Form.Field id = 'input-message' control ={TextArea} name = 'user_message' placeholder = 'Message...' required/>
-        <Button type = 'submit' color = 'green'>Send</Button>
+        <table className = 'tableForm'>
+          <tr>
+            <td colSpan={2}>
+              <label className = "label" for = "Name">Name: </label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Form.Field className = "input" id = 'input-name-first' control = {Input} name = 'user_name_first' placeholder = 'Enter Your First Name *' required icon = 'user circle' iconPosition = 'left'/>
+            </td>
+            <td>
+              <Form.Field className = "input" id = 'input-name-last' control = {Input} name = 'user_name_last' placeholder = 'Enter Your Last Name *' required icon = 'user circle' iconPosition = 'left'/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label className = "label" for = "Email">Email: </label>
+            </td>
+            <td>
+              <label className = "label" for = "Phone">Phone Number: </label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Form.Field className = "input" id = 'input-email' control = {Input} name = 'user_email' placeholder = 'Enter Your Email *' iconPosition = 'left' required/>
+            </td>
+            <td>
+              <Form.Field className = "input" id = 'input-phone' control = {Input} name = 'user_phone' placeholder = 'Enter Your Phone Number *' iconPosition = 'left'/>
+            </td>    
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <label className = "label" for = "Subject">Subject: </label>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <Form.Field className = "input" id = 'input-subject' control ={Input} name = 'user_subject' placeholder = 'Enter Subject Here *' required/>
+            </td>
+          </tr>
+          </table>
+
+          <table>
+          <tr>
+            <td colSpan={2}>
+              <label className = "label" for = "Message">Message: </label>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <Form.Field className = "input" id = 'input-message' control ={TextArea} name = 'user_message' placeholder = 'Enter Message Here *' required/>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+                <Button type = 'submit' color = 'green'>Send</Button>
+            </td>
+          </tr>
+        </table>
       </Form>
     </div>
   );
-}
+  }
 }
 export default Contact;
