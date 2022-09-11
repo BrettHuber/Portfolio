d111ccd4a120e982af276d526a238cf744a539fc
B H <hawktalon505@gmail.com>
2022-09-10 23:04:30 -0400

    Added download resume to home.js

--- portfolio-website/src/components/Home/Home.js
+++ portfolio-website/src/components/Home/Home.js
@@ -4,15 +4,29 @@ import './Home.css'
 
 
 class Home extends Component {
+
+    downloadClick = () => {
+        fetch('Brett_AWS_No_Column.pdf').then(response => {
+            response.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Brett_AWS_No_Column.pdf';
+                alink.click();
+            })
+            
+        })
+    }
+
     render() {
         return (
             <div className="home-container">
-                {/* <div className="text-center"> */}
                     <img src =  'images/headshot-bw.png' className = "headshot" alt = "Brett"></img>
                     <h1 className= "homeName">Hey. I'm Brett Huber</h1>
                     <h4 className="homeAttributes">Computing Security Student - Programmer - Security Analyst</h4>
-                    <p></p>
-                {/* </div> */}
+                    <hr></hr>
+                    <p>Python | Java | HTML/CSS | JavaScript | C | C++ | SQL</p>
+                    <button onLoadError={console.error} onClick = {this.downloadClick}>Download Resume</button>
             </div>
          
         );
