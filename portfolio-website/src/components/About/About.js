0e634424f6f81d9e5a530623fc6f16818892b034
B H <hawktalon505@gmail.com>
2022-09-12 15:33:15 -0400

    Updated about.js and about.css

--- portfolio-website/src/components/About/About.js
+++ portfolio-website/src/components/About/About.js
@@ -8,27 +8,46 @@ class About extends Component {
         return (
             <div className="about-container">
                     <h1 className= "aboutLabel">About Me</h1>
+                    
                     <img className = "photo" src = "images/Web-Portrait.jpg"></img>
-                    <h3 className= "skillsLabel">Blurb</h3>
-                    <p className = 'blurb'>
-                    Welcome to my website! So here is some information about me. My name is Brett Huber. I'm a 2nd/3rd year Computing Security student on the BS/MS track at Rochester Institute of Technology. 
-                    I have been involved in computer science since I was in middle school. I was accepted into Passaic County Technical Vocational School and completed a computer science concentration for all four years. 
-                    I have a deep love of many different computing fields, especially security. </p>
+                    
+                    <div className = 'blurbDiv'><h3 className= "skillsLabel">Blurb</h3>
+                        <p className = 'blurb'>
+                            Welcome to my website! So here is some information about me. My name is Brett Huber. I'm a 2nd/3rd year Computing Security student on the BS/MS track at Rochester Institute of Technology. 
+                            I have been involved in computer science since I was in middle school. 
+                            I was accepted into Passaic County Technical Vocational School and completed a computer science concentration for four years, covering a wide variety of topic including but not limited to databases, scripts, and web development. 
+                            Due to a deep love of many different computing fields, especially security, I have completed and worked on many projects using a wide variety of different skills such as software and web development, different operating systems, security protocols, and more.
+                            While I love technology, I am also big fan of the outdoors and nature. Spending time in nature provides me with a cathartic experience that clears my mind.
+                            Overall, I am a technology and outdoor oriented person with a significant drive for learning.
+                        </p>
+                    </div>
+                    
                     <h3 className= "skillsLabel">Hobbies</h3>
-                    <ul>
-                        <li>Biking (Road & Mountain)</li>
-                        <li>Boxing</li>
-                        <li>Camping</li>
-                        <li>Soccer</li>
-                        <li>Volleyball</li>
-                    </ul>
-                    <h3 className= "skillsLabel">Skills</h3>
-                    <hr></hr>
-                    <p>Python | Java | HTML/CSS | JavaScript | C | C++ | SQL</p>
-                    <hr></hr>
-                    <p>ReactJS | Wireshark | Git/Github | VMware | Nmap | Arduino | Cisco IOS | Adobe Photoshop | Soldering Iron</p>
-                    <hr></hr>
-                    <p>Microsoft Windows | Kali | Ubuntu</p>
+                        <hr></hr>
+                        <p className = "list">Programming | Security Scripting | Projects | Web Development | Soldering</p>
+                        <hr></hr>
+                        <p className = "list">Biking (Road & Mountain) | Boxing | Camping | Fishing | Climbing (Rock & Ice) | Fencing | Soccer | Curling | Volleyball</p>
+                    
+                    <br></br>
+                    <br></br>
+
+                    <img className = "act-images" src = "images/act-soccer.png" alt = "Soccer Icon"></img>
+                    <img className = "act-images" src = "images/act-boxing.png" alt = "Boxing Icon"></img>
+                    <img className = "act-images" src = "images/act-laptop.png" alt = "Laptop Icon"></img>
+                    <img className = "act-images" src = "images/act-fish.png" alt = "Fish Icon"></img>
+                    <img className = "act-images" src = "images/act-bicycle.png" alt = "Bicycle Icon"></img>
+
+                    <br></br>
+                    <br></br>
+
+                    <div><h3 className= "skillsLabel">Skills</h3>
+                        <hr></hr>
+                        <p className = "list">Python | Java | HTML/CSS | JavaScript | C | C++ | SQL</p>
+                        <hr></hr>
+                        <p className = "list">ReactJS | Wireshark | Git/Github | VMware | Nmap | Arduino | Cisco IOS | Adobe Photoshop | Soldering Iron</p>
+                        <hr></hr>
+                        <p className = "list">Microsoft Windows | Kali | Ubuntu</p>
+                    </div>
             </div>
         );
     }
